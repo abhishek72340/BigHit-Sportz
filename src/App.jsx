37ed87a069be09f42b1d6f84dca1e26bd190b239
abhishek <abhisingh.72340@gmail.com>
2024-04-28 07:33:56 +0530

    fix bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
+import { Suspense, lazy } from "react";
 import { Routes, Route } from "react-router-dom";
+import Loading from "./Components/Skeleton/Skeleton";
 import Home from "./Pages/Home/Home";
-import ProductDetails from "./Pages/ProductDetails/ProductDetails";
-import Cart from "./Pages/Cart/Cart";
-import NotFoundPage from "./Pages/NotFoundPage/NotFoundPage";
-import Wishlist from "./Pages/Wishlist/Wishlist";
-
+const ProductDetails = lazy(() =>
+  import("./Pages/ProductDetails/ProductDetails")
+);
+const Cart = lazy(() => import("./Pages/Cart/Cart"));
+const Wishlist = lazy(() => import("./Pages/Wishlist/Wishlist"));
+const NotFoundPage = lazy(() => import("./Pages/NotFoundPage/NotFoundPage"));
 const App = () => {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/products/:id" element={<ProductDetails />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/wishlist" element={<Wishlist />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products/:id" element={<ProductDetails />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/wishlist" element={<Wishlist />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Suspense>
     </>
   );
 };
